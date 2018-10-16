a4e312303afcf5f3accf8afc56a54e8060ca1ea3
Ednaldo <martinsednaldo@outlook.com>
2018-10-15 23:39:18 -0300

    Refatorando nome do projeto

--- src/problemadamochila/ProblemaDaMochila.java
+++ src/problemadamochila/ProblemaDaMochila.java
@@ -5,7 +5,7 @@ package problemadamochila;
  * @author Ednaldo                                                             *
  *  date: 11.10.2018                                                           *
  ******************************************************************************/
-public class OtimizacaoCombinatoria 
+public class ProblemaDaMochila 
 {
 
     public static void main(String[] args) 
