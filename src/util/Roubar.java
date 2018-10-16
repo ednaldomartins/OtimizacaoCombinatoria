1d72663ce7c9f1810d4365aadf4af24063652ea3
Ednaldo <martinsednaldo@outlook.com>
2018-10-16 12:25:02 -0300

    Interface para implementar o método de roubar itens

---
+++ src/util/Roubar.java
@@ -0,0 +1,16 @@
+
+package util;
+
+import java.util.List;
+
+import model.Item;
+import model.Mochila;
+
+/*******************************************************************************
+ * @author Ednaldo                                                             *
+ *  date: 16.10.2018                                                           *
+ ******************************************************************************/
+public interface Roubar 
+{
+    void roubarItem(List <Item> listaItens, long nItens);
+}
