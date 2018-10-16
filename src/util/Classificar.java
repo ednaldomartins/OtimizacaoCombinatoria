c5359f866d9d92c1db149f08095625a196a14c2c
Ednaldo <martinsednaldo@outlook.com>
2018-10-16 12:25:16 -0300

    Interface para implementar o método de classificar o valor dos itens

---
+++ src/util/Classificar.java
@@ -0,0 +1,15 @@
+
+package util;
+
+import java.util.List;
+
+import model.Item;
+
+/*******************************************************************************
+ * @author Ednaldo                                                             *
+ *  date: 16.10.2018                                                           *
+ ******************************************************************************/
+public interface Classificar 
+{
+    void classificarItens(List <Item> listaItens);
+}
