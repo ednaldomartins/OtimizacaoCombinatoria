35e92582a9e9debf4845f1d99bdfebaf9fa3d8d9
Ednaldo <martinsednaldo@outlook.com>
2018-10-16 00:22:09 -0300

    Primeiros testes

--- src/problemadamochila/ProblemaDaMochila.java
+++ src/problemadamochila/ProblemaDaMochila.java
@@ -1,6 +1,12 @@
 
 package problemadamochila;
 
+import infra.Arquivo;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import model.Item;
+
 /*******************************************************************************
  * @author Ednaldo                                                             *
  *  date: 11.10.2018                                                           *
@@ -8,8 +14,11 @@ package problemadamochila;
 public class ProblemaDaMochila 
 {
 
-    public static void main(String[] args) 
+    public static void main(String[] args) throws IOException 
     {
-        
+        Arquivo arquivo = new Arquivo("..//ProblemaDaMochila//src//infra//instancias//mochila01.txt");
+        List <Item> list = new ArrayList();
+        list = arquivo.carregarObejtos();
+        System.out.print("");
     }
 }
